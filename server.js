97230117a89abf27dd7ed261120ed8253620014d
Ubuntu <ubuntu@ip-172-31-20-255.us-east-2.compute.internal>
2018-10-31 03:36:28 +0000

    added create account

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const port = 3000;
 const dir = __dirname;
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config({path: '../env_variables.env'});
 const mysql = require('mysql').createConnection({
-  host: "umami.cuxpgdmnriur.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "snXGq6viFIpt"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 const app = express();
-
 mysql.connect((err) => {
   if(err) throw err;
   console.log("Connected to db");
@@ -29,14 +30,33 @@ app.get('/', (req, res) => {
   res.end('Hello World\n');
 })
 
-app.get('/createaccount', (req, res) => {
+app.post('/createaccount', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let fullName = req.body.name;
+  let checkUserExistsQuery = "SELECT * FROM users WHERE username = '" + username + "'";
+  let insertQuery = "INSERT INTO users (username, password, name) VALUES ('" + username + "', '" + password + "', '" + fullName + "')";
+  mysql.query(checkUserExistsQuery, (err, result) => {
+    if(err) {
+      throw err;
+    }
+    else if(result.length == 1) {
+      res.send("already a user created with this username");
+    }
+    else {
+      mysql.query(insertQuery, (err, result) => {
+        if (err) throw err;
+        res.send("Successfully added user");
+      })
+    }
+  })
+
+  // Probably going to need to change below appropriately
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('To be implemented\n');
 })
 
-app.post('/test', (req, res) => {
-  console.log("received button request");
+app.post('/login', (req, res) => {
   res.send({username: req.body.username, password: req.body.password});
 })
 
