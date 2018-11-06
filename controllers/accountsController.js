45c0eb8d5f4a42b29e7612a6a0832d13d5c85ab8
Ubuntu <ubuntu@ip-172-31-20-255.us-east-2.compute.internal>
2018-11-06 09:25:13 +0000

    basic signup, signin, and change password

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -1,39 +1,76 @@
-exports.test = (req, res) => {
-  res.send("/api/accounts");
-}
+require('dotenv').config({path: '../../env_variables.env'});
+const mysql = require('mysql').createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+const checkUserExistsQuery = "SELECT * FROM users WHERE username = ?";
 
+
+exports.test = (req, res) => {
+  let query = `SELECT * from users`;
+  mysql.query(query, (err, results) => {
+  if (err) {
+    res.status(500).json( {error: err} );
+  }
+  else {
+    res.status(200).json(results);
+  }
+     
+ });
+}
 /** Function for signing up 
  */
 exports.signup = (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let fullName = req.body.name;
-  let insertQuery = "INSERT INTO users (username, password, name) VALUES (?, ?, ?)";
-  mysql.query(checkUserExistsQuery,[req.body.username], (err, result) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const fullName = req.body.name;
+  
+  mysql.query(checkUserExistsQuery, [username], (err, result) => {
     if(err) {
       throw err;
     }
     else if(result.length == 1) {
-      res.send("already a user created with this username");
+      res.status(409).json( {error: "This username already exist"} );
     }
-    else {
-      mysql.query(insertQuery, [req.body.username, req.body.password. req.body.name], (err, result) => {
-        if (err) throw err;
-        res.send("Successfully added user");
-      })
+    else {      
+      let insertQuery = "INSERT INTO users (username, password, full_name) VALUES (?, ?, ?)";
+      mysql.query(insertQuery, [username, password, fullName], (err, result) => {
+        if (err) {
+          res.status(500).json( {erorr: err} );
+          throw err;
+        }
+        res.status(201).json( {message: "User created"} );
+      });
     }
-  })
+  });
 
-  // Probably going to need to change below appropriately
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
 }
 
 
 /** Function for signing in 
  */
 exports.signin = (req, res) => {
-  res.end('Not implemented');
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  mysql.query(checkUserExistsQuery, [username], (err, result) => {
+    if (err) {
+      res.status(500).json( {error: err} );
+      throw err;
+    }
+
+    if (result.length != 0) { // user exists
+      (result[0].password == password)? 
+        res.status(200).json( {message: "login successful" } ) :
+        res.status(401).json( {error: "incorrect password"} );
+    }
+    else { // user doesn't exists
+      res.status(404).json( {error: '\"'+username+'\" does not exist'} );
+    }
+  });
+
 }
 
 
@@ -53,14 +90,28 @@ exports.reset_password = (req, res) => {
       throw err;
     }
     mysql.query(getPasswordQuery, [req.body.username], (err, result) => {
-      if(result != req.body.oldPassword) {
-        // IMPLEMENT FAIL
+      if(result[0].password != req.body.oldPassword) { // old password incorrect
+        res.status(401).json( {error: "incorrect password"} );
       }
-      else {
+      else { // old password corrrect
+        if (req.body.newPassword1 != req.body.newPassword2) {
+          res.status(422).json( {error: "new passwords don't match; try again'"} );
+        }
+        else { // new passwords match
+          const password = req.body.newPassword1;
+       	  let updatePasswordQuery = `UPDATE users SET password='`+password+`'
+                                     WHERE username=?`;
+          mysql.query(updatePasswordQuery, [req.body.username], (err) => {
+          (err)?
+            res.status(500).json( {error: err} ) :
+            res.status(200).json( {message: "password changed"} ); 
+          }); 
+        }
+      }
 
-      }
-    })
-  })
+    });
+  });
+
 }
 
 /** Function for deleting account
