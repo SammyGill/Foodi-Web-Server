4186d647c11a5160ff424d447f5f24485fa423d3
Ubuntu <ubuntu@ip-172-31-20-255.us-east-2.compute.internal>
2018-11-27 07:55:03 +0000

    set username and get list of usernames

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -6,36 +6,7 @@ const mysql = require('mysql').createConnection({
   database: process.env.DB_NAME
 });
 const checkUserExistsQuery = "SELECT * FROM users WHERE user_id = ?";
-
-
-/** Function for signing up 
- */
-exports.signup = function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  const fullName = req.body.name;
-  
-  mysql.query(checkUserExistsQuery, [username], (err, result) => {
-    if(err) {
-      throw err;
-    }
-    else if(result.length == 1) {
-      res.status(409).json( {"Conflict": "This username already exist"} );
-    }
-    else {      
-      let insertQuery = "INSERT INTO users (username, password, full_name) VALUES (?, ?, ?)";
-      mysql.query(insertQuery, [username, password, fullName], (err, result) => {
-        if (err) {
-          res.status(500).json( {"Internal Service Error": err} );
-          throw err;
-        }
-        res.status(201).json( {"Created": "Account created"} );
-      });
-    }
-  });
-
-}
-
+const checkUsername = "SELECT * FROM users WHERE username=?";
 
 /** Function for signing in 
  */
@@ -52,76 +23,28 @@ exports.signin = (req, res) => {
       const fn = req.userData.first_name;
       const ln = req.userData.last_name;
       const pic = req.userData.picture.data.url;
+      const username = fn + ln + user_id;
       let insertQuery = 
-        `INSERT INTO users (user_id, first_name, last_name, profile_picture) 
-         VALUES (?, ?, ?, ?)`;
-      mysql.query(insertQuery, [user_id, fn, ln, pic], (err, result) => {
+        `INSERT INTO users (user_id, first_name, last_name, profile_picture, username) 
+         VALUES (?, ?, ?, ?, ?)`;
+      mysql.query(insertQuery, [user_id, fn, ln, pic, username], (err, result) => {
         if (err) {
-          res.status(500).json( {"Internal Service Error": err} );
+          res.status(500).json(err);
           throw err;
         }
-        res.status(200).json({ OK: "Login success", userData: req.userData });
+        res.status(201).json({ message: "Account created", userData: req.userData });
       });
     }
     else { // user exists; login successful
-      res.status(200).json( { OK: "Login success", userData: req.userData });
+      res.status(200).json( { message: "Login success", userData: req.userData });
     }
   });
 }
 
-
-/** Function for signing out
- */
-exports.signout = (req, res) => {
-  res.end("signout");
-}
-
-
-/** Function for resetting passsword 
- */
-exports.reset_password = (req, res) => {
-  let getPasswordQuery = "SELECT password FROM users WHERE username = ?";
-  mysql.query(checkUserExistsQuery, [req.body.username], (err, result) => {
-    if(err) {
-      throw err;
-    }
-    mysql.query(getPasswordQuery, [req.body.username], (err, result) => {
-      if(result[0].password != req.body.oldPassword) { // old password incorrect
-        res.status(401).json( {"Unauthorized": "Incorrect password"} );
-      }
-      else { // old password corrrect
-        if (req.body.newPassword1 != req.body.newPassword2) {
-          res.status(422).json( {"Unprocessable Entity": "New passwords don't match; try again'"} );
-        }
-        else { // new passwords match
-          const password = req.body.newPassword1;
-       	  let updatePasswordQuery = `UPDATE users SET password='`+password+`'
-                                     WHERE username=?`;
-          mysql.query(updatePasswordQuery, [req.body.username], (err) => {
-          (err)?
-            res.status(500).json( {"Internal Service Error": err} ) :
-            res.status(200).json( {"Success": "Password changed"} ); 
-          }); 
-        }
-      }
-
-    });
-  });
-
-}
 
 /** Function for deleting account
  */
 exports.delete_account = (req, res) => {
   res.end("delete account");
 }
-
-/**
- * allow user to change username if 
- * username is not taken
- */
-exports.change_username = (req, res) => {
-  red.end("change usernmae");
-}
-
 
--- routes/accounts.js
+++ routes/accounts.js
@@ -9,21 +9,12 @@ router.get('/test-auth', auth, (req, res) => {
   res.status(200).json(req.userData);
 });
 
-// POST request for signing up
-router.post('/signup', accounts_controller.signup);
 
 // POST request for signing in
 router.post('/signin', auth, accounts_controller.signin);
 
-// POSt request for signing out
-router.post('/signout', accounts_controller.signout);
-
-// POST request for resetting password
-router.post('/reset-password', accounts_controller.reset_password);
-
 // DELETE request for deleting account
-router.delete('/delete', accounts_controller.delete_account);
+router.delete('/delete', auth, accounts_controller.delete_account);
 
-//router.patch('/change-username', accounts_controller.change_usernmae);
 
 module.exports = router;
