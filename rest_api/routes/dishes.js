802fe6344ed52a872de9b53798df9ec921026e63
Peem Takoonsawat <mrpeem@gmail.com>
2018-12-02 00:49:10 -0800

    backend discovery for dish

---
+++ rest_api/routes/dishes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const dishes_controller = require('../controllers/dishesController');
+
+router.get('/list', dishes_controller.list);
+
+router.get('/search', dishes_controller.search);
+module.exports = router;
