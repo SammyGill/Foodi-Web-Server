905b0421724f6fdd19b1b34229f0662e63d67804
zhou yuwei <zhouyuwei@Yuweis-MacBook-Pro.local>
2018-12-01 17:39:59 -0800

    Unfollow

--- static/js/profile.js
+++ static/js/profile.js
@@ -24,3 +24,28 @@ function follow(user_id) {
   });
 
 }
+
+
+function unfollow(user_id) {
+  getCookie = (name) => {
+  var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return match[2];
+  }
+  const access_token = getCookie('accessToken');
+
+  $.ajax({
+    url: '/api/profiles/unfollow/' + user_id,
+    type: 'POST',
+    datetype: 'json',
+    beforeSend: (xhr) => {   //Include the bearer token in header
+      xhr.setRequestHeader("Authorization", 'Bearer '+ access_token);
+    },
+    success: (data, textStatus, xhr) => {
+      alert(JSON.stringify(data));
+    },
+    error: (err) => {
+      alert(JSON.stringify(err));
+    }
+  });
+
+}
