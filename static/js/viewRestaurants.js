c0db184b8e0e633aa2600843d3f01fd54b8af282
Peem Takoonsawat <mrpeem@gmail.com>
2018-12-20 21:41:23 -0800

    can share post

--- static/js/viewRestaurants.js
+++ static/js/viewRestaurants.js
@@ -1,15 +1,24 @@
 function showDish(dishName) {
   document.getElementById("galleries").hidden = true;
-  document.getElementById(dishName).hidden = false;
-// document.getElementById("galleries").style.display = "none";
+  
+  if ( document.getElementById(dishName) )
+    document.getElementById(dishName).removeAttribute("hidden");
+  else
+    $('#'+dishName).removeAttr("hidden");
 } 
 
 function hideDish(dishName) {
   document.getElementById("galleries").hidden = false;
-  document.getElementById(dishName).hidden = true;
-  // document.getElementById("galleries").style.display = "block";
+
+  let div;
+  if ( document.getElementById(dishName) )
+    div = document.getElementById(dishName);
+  else
+    div = $('#'+dishName);
+  div.hidden = true;
 }
 
+// takes user to view specific post
 $(document).ready( () => {
 	const url = window.location.href;
 
@@ -19,10 +28,14 @@ $(document).ready( () => {
     const arr = split[1].split('_');
     if (arr.length == 2) {
       const post_id = arr[0];
-      const dish_name = arr[1];
-      showDish(dish_name);
-      $('#'+post_id).click();
-
+      const dish_name = arr[1].replace( new RegExp( '%20', 'ig' ) , "\\ " )
+      
+      // check if valid dish name and post id were passed in
+      if ( $('#'+dish_name).length != 0 && $('#'+post_id).length != 0 ) {
+        showDish(dish_name); // show all posts for that dish
+        $('#'+post_id).click(); // click post to bring up modal
+      }
+      
     }
   }
 })
